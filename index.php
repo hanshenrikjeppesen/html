e75b1a712ef1d388ab9ec9d9689a97bdae0ee581
hans <hans@eaaa.dk>
2018-03-08 22:15:26 +0100

    clean up and start

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>RaspberryPi - Basement monitoring</title>
-<style>
-	body {background-color: powderblue;}
+	<title>RaspberryPi - learning PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <style>
+
+    body {background-color: powderblue;}
 	h1   {
 		
 		text-align: center;
@@ -12,18 +14,13 @@
 		font-size: 250%;
 	}
 	p    {color: red;}
-	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
 </style>
 </head>
 <body>
 	<h1>Project: Monitoring data in my home basement</h1>
 	<?php
-		$myfile =  fopen("/home/hans/temp_log_file.txt", "r") or die("can't finde the file on server");
-		while(! feof($myfile))
-  			{
-  			echo fgets($myfile). "<br />";
-			echo "<hr/ >";
-  			}
-		/* echo fread($myfile,filesize("/home/hans/temp_log_file.txt")); */
-		fclose($myfile);
+
 		?>
+</body>
+</html>
