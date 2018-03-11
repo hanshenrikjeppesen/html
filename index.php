337e6f98812ab1ffb8bc0f23a092869a2a85f21b
hanshenrikjeppesen <info@frist.dk>
2018-03-11 13:14:45 +0100

    using predefined variables

--- index.php
+++ index.php
@@ -18,11 +18,32 @@
 </style>
 </head>
 <body>
-	<h1>Project: Monitoring data in my home basement</h1>
+	<h1>Testing and Learning PHP</h1>
 	<?php
+	# Script version 1.1
+	# Created Marts 11 2018
+	# Created by Hans Henrik Jeppesen
+	# hans@eaaa.dk
+
+	// Creating new variables with predefined values
+	$file = $_SERVER['SCRIPT_FILENAME'];
+	$user = $_SERVER['HTTP_USER_AGENT'];
+	$server = $_SERVER['SERVER_SOFTWARE'];
+
+	//Print the name of the script;
+	echo "<h1>You are running thr file:</h1><br/><p><strong>$file</strong></p>\n";
+
+	//Print user's information;
+	echo "<h1>You are viewing this page using:</h1><br/><p><strong>$user</strong></p>\n";
+
+	//Print the server information to the browser;
+	echo "<h1>The server is running:</h1><br/><p><strong>$server</strong></p>\n";
+
+	/*
 	echo "This is a new line made in PHP";
 	echo "<h1>and another line with HTML tags</h1>";
 	echo "one more 'test'...";
+	*/
 		?>
 </body>
 </html>
