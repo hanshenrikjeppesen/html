7e9fe7d5bdc8f3d9f8c1d0d108e0b723ec43db08
hans <hans@eaaa.dk>
2018-03-15 15:38:23 +0100

    new script

--- script1_9.php
+++ script1_9.php
@@ -5,9 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="main.css">
-    <title>Script1_9</title>
+    <title>Script1_9 Constants</title>
 </head>
 <body>
+<div>
+<h1>Script 1.9 Constants</h1>
+</div>
+<?php
 
+// set todays date as a constant
+define('TODAY', 'Marts 15, 2018');
+
+//print a message using predefined constants and the TODAY constant
+echo '<p>Today is' . TODAY . '.<br>This server is running version <strong>' . PHP_VERSION . 
+'</strong> of PHP on the <strong>' . PHP_OS . '</strong> operating system.</p>'
+?>
 </body>
 </html>
