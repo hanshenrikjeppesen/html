677c02b0309fc6df355f39b940fd8a7d51a071f3
hans <hans@eaaa.dk>
2018-03-15 15:59:17 +0100

    next button

--- script1_9.php
+++ script1_9.php
@@ -20,5 +20,7 @@ define('TODAY', 'Marts 15, 2018');
 echo '<p>Today is' . TODAY . '.<br>This server is running version <strong>' . PHP_VERSION . 
 '</strong> of PHP on the <strong>' . PHP_OS . '</strong> operating system.</p>'
 ?>
+<h2>Next Script</h2>	
+	<h2><a href="script1_10.php" target="_blank">Script 1.10</a></h2>
 </body>
 </html>
