f45de7f00cd337de96b62aa8f8527a835873f488
hans <hans@eaaa.dk>
2018-03-15 23:00:00 +0100

    new cntl back to homepage

--- script1_9.php
+++ script1_9.php
@@ -21,6 +21,7 @@ echo '<p>Today is' . TODAY . '.<br>This server is running version <strong>' . PH
 '</strong> of PHP on the <strong>' . PHP_OS . '</strong> operating system.</p>'
 ?>
 <h2>Next Script</h2>	
-	<h2><a href="script1_10.php" target="_blank">Script 1.10</a></h2>
+    <h2><a href="script1_10.php" target="_blank">Script 1.10</a></h2>
+    <h2><a href="index.php" target="_blank">Frontpage</a></h2>
 </body>
 </html>
