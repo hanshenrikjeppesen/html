ba79c51d2ab508be9aa573c64642e75e332d1b21
hans <hans@eaaa.dk>
2018-03-16 11:18:28 +0100

    new line and table

--- script1_10.php
+++ script1_10.php
@@ -24,8 +24,52 @@ $total = $total + ($total * $taxrate);
 //format the number
 $total = number_format ($total, 2);
 
+//Text explaining the reason for this script
+echo "<h3>Why is it importent to know about Quotation marks?</h3>";
+echo "<p>In<strong>PHP</strong>, values enclosed within single quotation marks will be treated literally, 
+whereas those within double quotation marks will be interpreted</p>"
+echo "<p>Meaning placing variables and special characters (escape Sequences) within double quotes will result
+in their represented values printed, not their literal values"
+?>
+<table style="width:30%">
+  <tr>
+    <th>Code</th>
+    <th>Meaning</th> 
+  </tr>
+  <tr>
+    <td>\"</td>
+    <td>Double quotes</td>
+  </tr>
+  <tr>
+    <td>\'</td>
+    <td>Single quotes</td>
+  </tr>
+  <tr>
+    <td>\\</td>
+    <td>Backslash</td>
+  </tr>
+  <tr>
+    <td>\n</td>
+    <td>Newline</td>
+  </tr>
+  <tr>
+    <td>\r</td>
+    <td>Carriage return</td>
+  </tr>
+  <tr>
+    <td>\t</td>
+    <td>Tab</td>
+  </tr>
+  <tr>
+    <td>\$</td>
+    <td>Dollar sign</td>
+  </tr>
+</table>
+
+<?php
+
 //print the resukt using double quoation
-echo "<h3>Using double quoation marks:</h3>";
+echo "<h3>Using double quotation marks:</h3>";
 echo "<p>You are purchasing <strong>$quantity</strong> widget(s) at a cost of <strong>\$$price</strong> each- With tax, the total comes to <strong> \$$total</strong></p>\n";
 
 
