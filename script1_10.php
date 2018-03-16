6ce1fed5b9248a259e1a865e89846445d8963168
hans <hans@eaaa.dk>
2018-03-16 13:30:11 +0100

    spelling

--- script1_10.php
+++ script1_10.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="main.css">
-    <title>Script 1_10 quotaions and math</title>
+    <title>Script 1_10 quotations and math</title>
 </head>
 <body>
 <div class="topbox">
