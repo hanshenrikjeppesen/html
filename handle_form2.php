a63473b1562747e7f706c835db3c8bdf8c8927c6
hanshenrikjeppesen <info@frist.dk>
2018-03-18 13:27:17 +0100

    new conditional

---
+++ handle_form2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Form Feedback nr. 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php # Script 2.3 handle_form2.php
+
+// defining variables
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$comments = $_REQUEST['comments'];
+$age = $_REQUEST['age'];
+
+// Create the $gender variable;
+
+if (isset($_REQUEST['gender'])){
+    $gender = $_REQUEST['gender'];
+} else {
+    $gender = NULL;
+}
+
+// printing the information submitted information from the html form.
+
+echo "<h2>Hallo</br>$name</h2><p>thank you for the following comment</p>
+<pre>$comments</pre>
+<p>We wil get back to you on the following email: <em>$email</em></p>"; 
+
+// print a message based upon the gender value;
+
+if ($gender =='M') {
+    echo '<p><stong>Have a nice day, Sir!</strong></p>';
+} elseif ($gender == 'F') {
+    echo '<p><stong>Have a nice day, Madam!</strong></p>';
+} else {
+    // No gender is selected
+    echo '<p><stong>You forgot to enter your data</strong></p>';
+}
+
+?>
+</body>
+</html>
